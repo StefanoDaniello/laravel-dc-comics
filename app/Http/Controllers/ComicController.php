4922ea4508fb1031cc9faa1c6079c93dad8399b4
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-05-29 17:24:57 +0200

    add validation to edit

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -41,15 +41,16 @@ class ComicController extends Controller
     {   
 
 
-        $request->validate([
-            'title' => 'required|max:255|min:5',
-            'description' => 'nullable',
-            'thumb' => 'nullable|255',
-            'price' => 'required|max:255|min:1',
-            'series' => 'required|max:255',
-            'sale_date' => 'required|date|max:255',
-            'type' => 'required|max:20',
-        ]);
+        // $request->validate([
+        //     'title' => 'required|max:255|min:5',
+        //     'description' => 'nullable',
+        //     'thumb' => 'nullable|255',
+        //     'price' => 'required|max:255|min:1',
+        //     'series' => 'required|max:255',
+        //     'sale_date' => 'required|date|max:255',
+        //     'type' => 'required|max:20',
+        // ]);
+        $form_data= $this->validation($request->all());
         $form_data = $request->all();
         $new_comic = Comic::create($form_data);
         return redirect()->route('comics.index')->with('message', "Created {$new_comic->title} successfully");
@@ -77,7 +78,9 @@ class ComicController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, Comic $comic)
+
     {
+        $form_data= $this->validation($request->all());
         $form_data = $request->all();
         $comic->update($form_data);
         return redirect()->route('comics.index', $comic->id);
@@ -91,4 +94,34 @@ class ComicController extends Controller
         $comic->delete();
         return redirect()->route('comics.index')->with('message', "Deleted {$comic->title} successfully");
     }
+    
+    public function validation($data){
+        $validator = \Validator::make($data, [
+            'title' => 'required|max:255|min:5',
+            'description' => 'nullable',
+            'thumb' => 'nullable|max:255', 
+            'price' => 'required|numeric|min:1', 
+            'series' => 'required|max:255|min:1',
+            'sale_date' => 'required|date', 
+            'type' => 'required|max:20',
+        ],
+        [
+            'title.required' => 'Il titolo è obbligatorio.',
+            'title.max' => 'Il titolo non può superare i :255 caratteri.',
+            'title.min' => 'Il titolo deve essere di almeno : 5 caratteri.',
+            'thumb.max' => 'La lunghezza massima del thumbnail è :255 caratteri.',
+            'price.required' => 'Il prezzo è obbligatorio.',
+            'price.numeric' => 'Il prezzo deve essere un valore numerico.',
+            'price.min' => 'Il prezzo deve essere almeno :1.',
+            'series.required' => 'La serie è obbligatoria.',
+            'series.max' => 'La serie non può superare :max caratteri.',
+            'series.min' => 'La serie deve essere di almeno : 1 caratteri.',
+            'sale_date.required' => 'La data di vendita è obbligatoria.',
+            'sale_date.date' => 'La data di vendita non è valida.',
+            'type.required' => 'Il tipo è obbligatorio.',
+            'type.max' => 'Il tipo non può superare :20 caratteri.',
+        ])->validate();
+        return $validator;
+    }
+    
 }
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -9,39 +9,61 @@
         @method('PUT')
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="titleHelp" name="title"
-             required value="{{$comic->title}}">
+            <input type="text" class="form-control  @error('title') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="titleHelp" name="title"
+              value="{{$comic->title}}">
+              @if($errors->has('title'))
+                <div class ="alert alert-danger">{{$errors->first('title')}}</div>
+             </div>
+            @endif
             <div id="titleHelp" class="form-text">Inserisci titolo</div>
          </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10" class="form-control">
+            <textarea name="description" id="description" cols="30" rows="10" class="form-control @error('description') is-invalid @enderror">
                 {{$comic->description}}
             </textarea>
+            @if($errors->has('description'))
+                <div class ="alert alert-danger">{{$errors->first('description')}}</div>
+            @endif
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label">image</label>
-            <input type="text" class="form-control" id="image" name="thumb" value="{{$comic->thumb}}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="image" name="thumb" value="{{$comic->thumb}}">
+            @if($errors->has('thumb'))
+                <div class ="alert alert-danger">{{$errors->first('thumb')}}</div>
+            @endif
          </div>
          <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" id="weight" name="price" required value="{{$comic->price}}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="weight" name="price"  value="{{$comic->price}}">
+            @if($errors->has('price'))
+                <div class ="alert alert-danger">{{$errors->first('price')}}</div>
+            @endif
          </div>
          <div class="mb-3">
             <label for="series" class="form-label">Serie</label>
-            <input type="text" class="form-control" id="cooking_time"  name="series" required value="{{$comic->series}}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="cooking_time"  name="series"  value="{{$comic->series}}">
+            @if($errors->has('series'))
+                <div class ="alert alert-danger">{{$errors->first('series')}}</div>
+            @endif
          </div>
          <div class="mb-3">
             <label for="scale_date" class="form-label">Scale date</label>
-            <input type="date" class="form-control" id="cooking_time"  name="sale_date" required value="{{$comic->sale_date}}">
+            <input type="date" class="form-control @error('scale_date') is-invalid @enderror" id="cooking_time"  name="sale_date"  value="{{$comic->sale_date}}">
+            @if($errors->has('scale_date'))
+                <div class ="alert alert-danger">{{$errors->first('scale_date')}}</div>
+            @endif
          </div>
           <div class="mb-3">
             <label for="type" class="form-label">Tipo</label>
-            <select class="form-control" id="type"  name="type" value="{{$comic->type}}" required>
-              <option  value="corta" {{$comic->type === 'comic book' ? 'selected' : ''}}>comic book</option>
-              <option value="lunga"  {{$comic->type === 'graphic novel' ? 'selected' : ''}}>graphic novel</option>
-              <option value="cortissima" {{$comic->type === 'other' ? 'selected' : ''}}>other</option>
+            <select class="form-control @error('type') is-invalid @enderror" id="type"  name="type" value="{{$comic->type}}" >
+              <option  value="comic book" {{$comic->type === 'comic book' ? 'selected' : ''}}>comic book</option>
+              <option value="graphic novel"  {{$comic->type === 'graphic novel' ? 'selected' : ''}}>graphic novel</option>
+              <option value="other" {{$comic->type === 'other' ? 'selected' : ''}}>other</option>
             </select>
+            @if($errors->has('type'))
+                <div class ="alert alert-danger">{{$errors->first('type')}}</div>
+            @endif
           </div>
         <div class="container text-center my-5">
           <button type="submit" class="btn btn-primary mx-4">Modifica</button>
